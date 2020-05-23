f56812d067ad6d2b45d700c21d3ce3e5fbb69892
Matvei Tikka <matvei@Matveis-MBP.home>
2020-05-23 17:35:24 +0300

    Removed blur effect, minor fixes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ![Screenshot](screenshot.png)
 Demo: https://matikka96.github.io/tic-tac-toe/
 ## About
-Simple game made using fundamental web development tools: HTML, CSS and JavaScript. It is designed for two individuals to play against each other. Game not only works on desktops, but also adapts to mobile devices.
+Simple game made using fundamental web development standards: HTML, CSS and JavaScript. It is designed for two individuals to play against each other. Game not only works on desktop browsers, but also adapts to mobile devices.
 
 ## How it works
-HTML & CSS handles design and responsiveness of the game. JavaScript on the other hand all the game related functions. It triggers notification element and also makes some minor changes in CSS styling.
+HTML & CSS handles design and responsiveness of the game. JavaScript handles all the game related logical functions. It triggers notification element and also makes some minor changes in CSS styling.
--- docs/scripts.js
+++ docs/scripts.js
@@ -1,72 +1,79 @@
-
 // Function scans 'grid' for possible winning combinations
 function scanForStrike() {
-	let result = 0;			// 0 = winning combination not found, 1 = found
-	let num1, num2, num3;
-	for (var i = 0; i < outcomes.length; i++) {
-		num1 = outcomes[i][0]-1;
-		num2 = outcomes[i][1]-1;
-		num3 = outcomes[i][2]-1;
-		if (grid[num1] == grid[num2] && grid[num1] == grid[num3] && grid[num1] != 0) {
-			result = 1;
-			continue;
-		}
-	}
-	return result;
+  let result = 0; // 0 = winning combination not found, 1 = found
+  let num1, num2, num3;
+  for (var i = 0; i < outcomes.length; i++) {
+    num1 = outcomes[i][0] - 1;
+    num2 = outcomes[i][1] - 1;
+    num3 = outcomes[i][2] - 1;
+    if (grid[num1] == grid[num2] && grid[num1] == grid[num3] && grid[num1] != 0) {
+      result = 1;
+      continue;
+    }
+  }
+  return result;
 }
 
 // Function displays notification. Parameters define message text and button text.
 function messageOpen(messageText, messageButton) {
-	document.getElementById("message-text").innerHTML = messageText;
-	document.getElementById("message-button").innerHTML = messageButton;
-	document.getElementById("message").style.display = "flex";
-	document.getElementById("game").style.filter = "blur(15px)";
+  document.getElementById("message-text").innerHTML = messageText;
+  document.getElementById("message-button").innerHTML = messageButton;
+  document.getElementById("message").style.display = "flex";
+  document.getElementById("game").style.opacity = "0.4";
 }
 // Function closes notification
 function messageClose(status) {
-	if (document.getElementById("message-button").textContent == 'reload') {
-		window.location.reload();
-	}
-	document.getElementById("message").style.display = "none";
-	document.getElementById("game").style.filter = "blur(0px)";
+  if (document.getElementById("message-button").textContent == "reload") {
+    window.location.reload();
+  }
+  document.getElementById("message").style.display = "none";
+  document.getElementById("game").style.opacity = "1";
 }
 
 // Main function, that handles all the visuals and game engine.
 function myFunction(index) {
-	if (grid[index] != 0) {
-		messageOpen("no can do", "ok");
-		return;
-	}
-	document.getElementsByTagName("button")[index].innerHTML = player;
-	var tempPlayer = player;
-	if (player == "X") {
-		grid[index] = 1;
-		document.getElementsByTagName("button")[index].style.backgroundColor = "tomato";
-		player="O";
-		document.getElementById("player").innerHTML = "player " + player;
-	} else {
-		grid[index] = 2;
-		document.getElementsByTagName("button")[index].style.backgroundColor = "limegreen";
-		player = "X";
-		document.getElementById("player").innerHTML = "player " + player;
-	}
-	var result = scanForStrike();	// Check for winning combination
-	if (result == 1) {
-		messageOpen("player "+tempPlayer+" won!", "reload"); 
-	}
-	if (grid.includes(0) == false) {
-		messageOpen("draw", "reload"); 
-	}
+  if (grid[index] != 0) {
+    messageOpen("no can do", "ok");
+    return;
+  }
+  document.getElementsByTagName("button")[index].innerHTML = player;
+  var tempPlayer = player;
+  if (player == "X") {
+    grid[index] = 1;
+    document.getElementsByTagName("button")[index].style.backgroundColor = "tomato";
+    player = "O";
+    document.getElementById("player").innerHTML = "player " + player;
+  } else {
+    grid[index] = 2;
+    document.getElementsByTagName("button")[index].style.backgroundColor = "limegreen";
+    player = "X";
+    document.getElementById("player").innerHTML = "player " + player;
+  }
+  var result = scanForStrike(); // Check for winning combination
+  if (result == 1) {
+    messageOpen("player " + tempPlayer + " won!", "reload");
+  }
+  if (grid.includes(0) == false) {
+    messageOpen("draw", "reload");
+  }
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 // Game starts with player "X"
 var player = "X";
 
 // Empty grid
-var grid = [0,0,0,0,0,0,0,0,0];
+var grid = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 // All possible winning combinations, total of 8.
-var outcomes = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], 
-				[2,5,8], [3,6,9], [1,5,9], [7,5,3]];
+var outcomes = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [7, 5, 3],
+];
